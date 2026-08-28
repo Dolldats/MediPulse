f3b61664246c468bc1535dcfd06f744fc18a36b8
Dolldats <dollarsakintola@gmail.com>
2026-08-28 19:53:26 +0100

    Patient interface partially done

---
+++ Application/Interfaces/Repositories/IPatientRepositories.cs
@@ -0,0 +1,20 @@
+﻿using MediPulse.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediPulse.Application.Interfaces.Repositories
+{
+    public interface IPatientRepositories
+    {
+        Task<IEnumerable<Patient>> GetAllPatientsAsync();
+        Task<Patient> GetPatientByIdAsync(int id);
+        Task<Patient> GetPatientByEmailAsync(string email);
+        Task AddAsync (Patient patient);
+        void UpdateAsync(Patient patient);
+        void DeleteAsync (Patient patient);
+        Task SaveChangesAsync();
+    }
+}
---
+++ Infrastructure/Data/MediPulseDbContext.cs
@@ -0,0 +1,27 @@
+﻿using MediPulse.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediPulse.Infrastructure.Data
+{
+    public class MediPulseDbContext : DbContext
+    {
+        public MediPulseDbContext(DbContextOptions<MediPulseDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MediPulseDbContext).Assembly);
+        }
+    }
+}
