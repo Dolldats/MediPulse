dcfb4b384d9814801f084f9ac5184f8a50634f80
Dolldats <dollarsakintola@gmail.com>
2026-08-29 21:48:17 +0100

    Doctor entity started

---
+++ Domain/Entities/Doctor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediPulse.Domain.Entities
+{
+    public class Doctor
+    {
+
+    }
+}
